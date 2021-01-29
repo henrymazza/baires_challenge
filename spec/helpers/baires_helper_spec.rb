d2518c235867fdc8e56e7b1f3ad29677cb937f70
Fabio Mazarotto <fabio@mazarotto.me>
2021-01-29 01:18:48 -0300

    Stack based parser

--- spec/helpers/baires_helper_spec.rb
+++ spec/helpers/baires_helper_spec.rb
@@ -1,64 +1,71 @@
 require 'rails_helper'
 
-
-RSpec.describe 'Parenthesis Challenge' do
-
+RSpec.describe 'Brackets Pairs Challenge' do
   def balanced(str)
-    parent = 0
-    square = 0
-    curly = 0
-
     return false if str.empty?
 
-    str.split('').each { |c|
-      case c
-      when "}"
-        curly = curly - 1
-      when "{"
-        curly = curly + 1
-      when ")"
-        parent = parent - 1
-      when "("
-        parent = parent + 1
-      when "]"
-        square = square - 1
-      when "["
-        square = square + 1
+    pairs = [
+      ['(', ')'],
+      ['[', ']'],
+      ['{', '}']
+    ]
+    opener = pairs.map { |pair| pair[0] }
+    closer = pairs.map { |pair| pair[1] }
+
+    stack = []
+
+    str.split('').each do |c|
+      if opener.include? c
+        stack.append c
+      elsif closer.include? c
+        # if char forms a pair with last element of stack
+        if pairs.reject { |p| p == [stack.last, c] }.size < 3
+          stack.pop
+        # pair mismatch otherwise
+        else
+          return false
+        end
       end
-      return false if square != 0 || curly != 0 || parent != 0
-    }
-    true
+    end
+
+    stack.empty?
   end
 
-   describe 'index page' do
-    it 'shows the right content' do
-      expect(balanced("(a[0]+b[2c[6]]) {24 + 53}")).to be true
+  describe '#balanced' do
+    context 'when pairs are balanced' do
+      it 'returns true' do
+        expect(balanced('(a[0]+b[2c[6]]) {24 + 53}')).to be true
+      end
+      it 'returns true' do
+        expect(balanced('f(e(d))')).to be true
+      end
+      it 'returns true' do
+        expect(balanced('[()]{}([])')).to be true
+      end
     end
-    it 'shows the right content' do
-      expect(balanced "f(e(d))").to be true
-    end
-    it 'shows the right content' do
-      expect(balanced("[()]{}([])")).to be true
-    end
-    it 'shows the right content' do
-      expect(balanced("((b) ")).to be false
-    end
-    it 'shows the right content' do
-      expect(balanced "(c] ").to be false
-    end
-    # it 'shows the right content' do
-    #   expect({(a[]) ).to be false
-    #       end
-    #         it 'shows the right content' do
-    #           expect(([)] ).to be false
-    #         end
-    #   it 'shows the right content' do
-    # it 'shows the right content' do
-    #   expect(")( ").to be false
-    # end
-    it 'shows the right content' do
-      expect(balanced("")).to be false
-    end
-   end
 
+    context 'when pairs are not balanced' do
+      it 'returns false' do
+        expect(balanced('((b) ')).to be false
+      end
+      it 'returns false' do
+        expect(balanced('(c] ')).to be false
+      end
+      it 'returns false' do
+        expect(balanced('{(a[])')).to be false
+      end
+      it 'returns false' do
+        expect(balanced('([)]')).to be false
+      end
+      it 'returns false' do
+        expect(balanced(')( ')).to be false
+      end
+    end
+
+    context 'when string is empty' do
+      it 'returns false' do
+        expect(balanced('')).to be false
+      end
+    end
+  end
 end
